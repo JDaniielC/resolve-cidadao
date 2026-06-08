d879d2c0c29448590d56ed01ee5e576cfb7388d7
Rayhene Ranúzia <rayhene@beehome.company>
2026-06-08 07:40:42 -0300

    feat: Adiciona background do menu (#8)

--- scenes/levels/area_2d.gd
+++ scenes/levels/area_2d.gd
@@ -9,4 +9,3 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("[Area2D] Player entered school area. Transitioning automatically to: %s" % target_scene)
 		MenuController.load_scene(target_scene)
-
