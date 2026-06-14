f7bcd1b7643f79523ba0790bb406c9d2d0372746
Rayhene <rayhene@beehome.company>
2026-06-14 17:48:11 -0300

    feat: adicionar limites de câmera nos mapas e refinar colisões do mapa 1

--- scenes/system/game_camera.gd
+++ scenes/system/game_camera.gd
@@ -2,6 +2,11 @@
 class_name GameCamera
 extends Camera2D
 
+const DEFAULT_LIMIT_LEFT := -10000000
+const DEFAULT_LIMIT_TOP := -10000000
+const DEFAULT_LIMIT_RIGHT := 10000000
+const DEFAULT_LIMIT_BOTTOM := 10000000
+
 @export var target_manager: TargetManager
 
 func _ready() -> void:
@@ -23,6 +28,25 @@ func _enable_smoothing(value):
 func _follow_target():
 	if target_manager:
 		global_position = target_manager.get_target_position()
+
+func apply_level_bounds(level: Node) -> void:
+	var bounds_node := level.find_child("CameraBounds", true, false) as CameraBounds
+	if bounds_node:
+		_set_limits(bounds_node.get_world_bounds())
+	else:
+		clear_limits()
+
+func clear_limits() -> void:
+	limit_left = DEFAULT_LIMIT_LEFT
+	limit_top = DEFAULT_LIMIT_TOP
+	limit_right = DEFAULT_LIMIT_RIGHT
+	limit_bottom = DEFAULT_LIMIT_BOTTOM
+
+func _set_limits(rect: Rect2) -> void:
+	limit_left = int(rect.position.x)
+	limit_top = int(rect.position.y)
+	limit_right = int(rect.end.x)
+	limit_bottom = int(rect.end.y)
 
 func refresh_target() -> void:
 	if target_manager:
